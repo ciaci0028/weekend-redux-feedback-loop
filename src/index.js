bf23ea23bb5f57189c54a8d9d6e785a46912e170
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-01-20 18:51:13 -0600

    created all reducers needed

--- src/index.js
+++ src/index.js
@@ -11,11 +11,45 @@ import { useEffect } from 'react';
 import logger from "redux-logger";
 
 // Reducers
+const feelingInput = (state, action) => {
+    switch(action.type) {
+        case 'INPUT_FEELING':
+            return action.payload
+    }
+    return state;
+};
+
+const understandingInput = (state, action) => {
+    switch(action.type) {
+        case 'INPUT_UNDERSTANDING':
+            return action.payload
+    }
+    return state;
+};
+
+const supportInput = (state, action) => {
+    switch(action.type) {
+        case 'INPUT_SUPPORT':
+            return action.payload
+    }
+    return state;
+};
+
+const commentsInput = (state, action) => {
+    switch(action.type) {
+        case 'INPUT_COMMENTS':
+            return action.payload
+    }
+    return state;
+};
 
 // Create the store
 const store = createStore(
     combineReducers({
-
+        feelingInput,
+        understandingInput,
+        supportInput,
+        commentsInput,
     }),
     applyMiddleware(logger)
 );
