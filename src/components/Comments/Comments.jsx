4605a0a2f1c97d4c7ace5da3ec9e3f6b9d33c561
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-01-20 20:54:20 -0600

    completed comments section and it sends payload back to reducer

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,9 +1,53 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import '../App/App.css';
+import { useState } from 'react';
+import {useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+
 function Comments () {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [comments, setComments] = useState('');
+
+    const sendComments = () => {
+        dispatch({
+            type: 'INPUT_COMMENTS',
+            payload: comments
+        })
+
+        history.push('/review');
+
+    }
+
     return (
-        <>
+        <div className="container">
             <h1>Do you have any comments?</h1>
-        </>
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+            <div>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Comments"
+                    multiline
+                    rows={4}
+                    onChange={event => setComments(event.target.value)}
+                />
+            </div>
+            </Box>
+            <br/><br/>
+            <Button onClick={event => sendComments(event)} variant="contained">Submit</Button>
+        </div>
     )
 };
 
 export default Comments;
+
